f0189c365305cf36604c067a2b1f5f307ccd0a9a
ankoks <koksharov.work@gmail.com>
2018-10-18 21:35:16 +0300

    REPO1. First commit

--- src/main/java/TestClassRepo1.java
+++ src/main/java/TestClassRepo1.java
@@ -2,5 +2,7 @@ public class TestClassRepo1 {
 
     public static void main(String[] args) {
         System.out.println("repo1");
+
+        System.out.println("some changes for repo 1");
     }
 }
